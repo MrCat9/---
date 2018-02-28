5f22a59134ac28dc6f93ee799f022acafcc590d8
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-02-28 15:45:29 +0800

    Update html_outputer.py

--- html_outputer.py
+++ html_outputer.py
@@ -1,14 +1,14 @@
 class HtmlOutputer(object):
-    def __init__(self):
+    def __init__(self): #初始化
         self.datas = []
     
     def collect_data(self, data):
         if data is None:
             return
-        self.datas.append(data)
+        self.datas.append(data) #在列表末尾添加新的对象
 
     def output_html(self):
-        fout = open('output.html', 'w', encoding = 'utf-8')
+        fout = open('output.html', 'w', encoding = 'utf-8') #以写形式打开'output.html'，编码为'utf-8'
         
         fout.write("<html>")
         fout.write("<body>")
@@ -26,5 +26,3 @@ class HtmlOutputer(object):
         fout.write("</html>")
         
         fout.close()        
-    
-    
